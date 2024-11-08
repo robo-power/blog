96fafccf74759c9f04fa3a2ffd7ed2b9a11fe308
robo-power <76481058+robo-power@users.noreply.github.com>
2024-11-08 21:13:32 +0900

    Added footer content. Removed test pages.

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { Inter } from 'next/font/google';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faXTwitter } from "@fortawesome/free-brands-svg-icons";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,8 +20,15 @@ export default function RootLayout({ children }) {
 	  		<a href="/"><h1> ロボのブログ</h1></a>
 	  	</header>
 	  	{children}
-	  	<footer className='text-center py-1'>
-	  		<a href="https://x.com/robo_power"><p>@robo_power</p></a>
+	  	<footer className='py-1'>
+			<div className='px-10 mx-auto max-w-screen-md'>
+				<div className='flex justify-center items-center gap-8'>
+					<div className='flex flex-row items-center gap-1'><FontAwesomeIcon icon={ faXTwitter } className="h-[20px]"/><a href="https://x.com/robo_power">@robo_power</a></div>
+					<div className='flex flex-row items-center gap-1'><FontAwesomeIcon icon={ faGithub } className="h-[20px]"/><a href="https://github.com/robo-power">@robo-power</a></div>
+					<a href="https://github.com/robo-power/blog">ブログのソース: robo-power/blog</a>
+				</div>
+				<p className='text-center'>©2024 robo_power</p>
+			</div>
 	  	</footer>
 	  </div>
       </body>
--- content/Second.md
+++
@@ -1,9 +0,0 @@
----
-title: Test Page 2
-date: "2024-11-05"
-description: "This is my second blog post!"
----
-
-## 見出し
-
-This is my second blog post!
